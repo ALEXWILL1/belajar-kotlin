3aaecf1d94b779fba598f518fa1ee1bef5a1a6d0
alex <alexanderwilliam189@gmail.com>
2026-09-07 13:48:43 +0700

    tugas 3B kayaknya

--- src/tugas4B.kt
+++ src/tugas4B.kt
@@ -8,6 +8,19 @@ fun main() {
     print("Masukan jumlah barang = ")
     val jumlahBarang = readln().toInt()!!
 
+    print("Masukan Diskon (%) = ")
+    val diskonBarang = readln().toDouble()!!
 
+    val subTotal = hargaBarang * jumlahBarang
+    val potongan = subTotal * diskonBarang / 100
+    val totalBayar = subTotal - potongan
 
+    println("")
+    println("nama barang kamu = $namaBarang")
+    println("harga barang satuab = $hargaBarang")
+    println("jumlah barang  = $jumlahBarang")
+    println("subTotal barang = $subTotal")
+    println ("diskon ($diskonBarang) = $potongan")
+    println("------------------------------------------")
+    println("total bayar = $totalBayar")
 }
