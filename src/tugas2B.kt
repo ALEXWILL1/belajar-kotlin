f2b6530fc61d6cfb81445d74d3bd7dc11538ca6b
alex <alexanderwilliam189@gmail.com>
2026-09-07 13:32:33 +0700

    tugas 4B modifikasi

--- src/tugas2B.kt
+++ src/tugas2B.kt
@@ -1,5 +1,5 @@
 fun main() {
-
+    
     var a = 10
     var b = 20
 
---
+++ src/tugas4B.kt
@@ -0,0 +1,13 @@
+fun main() {
+    print("masukan nama barang =")
+    val namaBarang = readLine()!!
+
+    print("Masukan harga barang =")
+    val hargaBarang = readln().toDouble()!!
+
+    print("Masukan jumlah barang = ")
+    val jumlahBarang = readln().toInt()!!
+
+
+
+}
