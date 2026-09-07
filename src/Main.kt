d6484735a48724818cd20b1c8091195a0fe91cda
alex <alexanderwilliam189@gmail.com>
2026-09-07 11:08:44 +0700

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val nama: String = "Alexander"
+    val umur: Int = 20
+    val tinggi: Double = 180.5
+    val aktif: Boolean = true
+
+    println("nama= $nama")
+    println("umur = $umur")
+    println("tinggi = $tinggi")
+    println("mahasiswa = $aktif")
+    println("halo, nama saya $nama")
+    println("Umur say $umur")
+}
